45deaf71013a58e9b2c02369d0fba264f0579bff
Zach Miller <zach.miller@wustl.edu>
2017-10-09 12:56:35 -0500

    Almost done

---
+++ debounce/debounce.ino
@@ -0,0 +1,11 @@
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  if (Serial.available()){
+    char myVar = Serial.read();
+    Serial.println(myVar);
+  }
+}
+
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -26,11 +26,55 @@ public class SerialComm {
 		debug = false; // Default is to NOT be in debug mode
 	}
 		
-	// TODO: Add writeByte() method from Studio 5
+	// TODO: Add writeByte() method to write data to serial port
+	void writeByte(byte a) {
+		try {
+			port.writeByte(a);
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (debug) {
+			System.out.println(Integer.toHexString(a));
+		}
+	}
+	
 	
 	// TODO: Add available() method
+	boolean available() {
+		try {
+			if (port.getInputBufferBytesCount() >= 1) {
+				return true;
+			}
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} return false;
+	}
 	
 	// TODO: Add readByte() method	
+	byte readByte() {
+		try {
+			return(port.readBytes(1)[0]);
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} return 0;
+	}
 	
-	// TODO: Add a main() method
+	public static void main(String [ ] args){
+		
+		SerialComm myComm = null;
+		try {
+			myComm = new SerialComm("COM3");
+		} catch (SerialPortException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (1==1) {
+			if (myComm.available()) {
+				System.out.println((char)myComm.readByte());
+			}
+		}
+	}
 }
