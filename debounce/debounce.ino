386c166bf187e7410f5743ff0e67b1f7051bf018
Zach Miller <zach.miller@wustl.edu>
2017-10-13 14:28:53 -0500

    Update debounce.ino

--- debounce/debounce.ino
+++ debounce/debounce.ino
@@ -1,11 +1,28 @@
-void setup(){
-  Serial.begin(9600);
-}
+const int buttonPin = 2;
+int count = 0;
+int test = 0;
+unsigned long timer = 0;
+const int betweenPresses = 100;
 
-void loop(){
-  if (Serial.available()){
-    char myVar = Serial.read();
-    Serial.println(myVar);
+void buttonPressed() {
+  if (millis() - timer >= betweenPresses){
+    count++;
+    timer = millis();
   }
 }
 
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Interrupts can happen on "edges" of signals.  
+  // Three edge types are supported: CHANGE, RISING, and FALLING 
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+    if (count > test) {
+      Serial.println(count);
+      test++;
+    }
+  }
